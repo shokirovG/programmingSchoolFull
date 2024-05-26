b4faf21ee77da47e0b2e41506de6f4063aa0000a
asdsada <sadsadda>
2024-05-26 07:57:24 +0500

    sadad

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -58,7 +58,6 @@ class UserController {
         SameSite: "none",
         secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
       });
 
       return res.json(userData);
