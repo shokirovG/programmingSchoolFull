067f4dcb5d4e23c3c6da75812032c2c54e23c5ec
asdsada <YOUR_EMAIL>
2025-05-19 13:13:12 +0500

    hotira oshirildi json

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const Register = require("./models/Register");
 const cookieParser = require("cookie-parser");
 const router = require("./router/auth");
 dotEnv.config();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.URL_FRONT); // Укажите домен клиента
   res.header(
